52a269fdb7b268687d2787d765d480310568cbf9
kangchanwook <kan0202@naver.com>
2023-08-13 01:47:12 +0900

    initial

---
+++ src/main/java/team/remember/RememberApplication.java
@@ -0,0 +1,13 @@
+package team.remember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RememberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RememberApplication.class, args);
+	}
+
+}
